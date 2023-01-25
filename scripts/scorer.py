e252020af5807647c6bb851cbda8dd3d1711719d
sob1eski <sobieski.bartlomiej.jan@gmail.com>
2023-01-25 13:10:41 +0100

    Add option for saving results to a general .csv.

--- scripts/scorer.py
+++ scripts/scorer.py
@@ -1,14 +1,17 @@
+import os
 import numpy as np
+import pandas as pd
 
 class Scorer():
 
-    def __init__(self, fitted_model, x_test, y_test, class_encoding):
+    def __init__(self, fitted_model, x_test, y_test, class_encoding, save_path):
         self.model = fitted_model
         self.x_test = x_test
         self.y_test = y_test
         self.encoding = class_encoding
+        self.save_path = save_path
     
-    def get_accuracies(self):
+    def get_accuracies(self, save = False, save_tags = {}):
         accuracies = {}
         accuracies['Full'] = self.model.score(
             self.x_test.values, 
@@ -18,6 +21,10 @@ class Scorer():
             features = self.x_test.values[idxs]
             labels = [class_id] * len(features)
             accuracies[self.encoding[class_id]] = self.model.score(features, labels)
+        if save:
+            save_tags.update(accuracies)
+            df = pd.DataFrame(save_tags, index = [0])
+            df.to_csv(self.save_path, mode = 'a', header = not os.path.exists(self.save_path))
         return accuracies
 
     
